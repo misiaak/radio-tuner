3dc27b310ad93e414d1788f7d13edf3998c165eb
Dominika Kucharska <dominika.kucharska@gmail.com>
2018-03-13 12:09:01 +0100

    Add propTypes to RadioStations component

--- src/components/RadioStations/RadioStation/index.jsx
+++ src/components/RadioStations/RadioStation/index.jsx
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class RadioStation extends Component {
   render () {
     const { station } = this.props
-    
+
     return (
       <li>{station.name}</li>
     )
   }
 }
 
+RadioStation.defaultProps = {
+  station: {},
+}
+
+RadioStation.propTypes = {
+  station: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+  }) 
+}
 export default RadioStation
